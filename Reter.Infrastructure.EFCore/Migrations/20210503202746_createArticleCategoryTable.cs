12c55b981e65914372e9d280b6c90d6c8c70de02
Alirexaa <Alireza.Baloochi1380@gmail.com>
2021-05-04 01:02:18 +0430

    Create Db and Add ArticleCategoryTable

---
+++ Reter.Infrastructure.EFCore/Migrations/20210503202746_createArticleCategoryTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reter.Infrastructure.EFCore.Migrations
+{
+    public partial class createArticleCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArticleCategories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleCategories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleCategories");
+        }
+    }
+}

abbe976c7f33e32047d88b24b132d30ff2b76c82
Alirexaa <Alireza.Baloochi1380@gmail.com>
2021-05-09 14:56:51 +0430

    user domain structure created

---
+++ Reter.Domain/UserAgg/Statuses.cs
@@ -0,0 +1,10 @@
+﻿namespace Reter.Domain.UserAgg
+{
+    public class Statuses
+    {
+        public const int New = 0;
+        public const int Active = 1;
+        public const int DeActive = 2;
+        public const int Ben = 3;
+    }
+}
---
+++ Reter.Domain/UserAgg/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Public.Framework.Domain;
+
+namespace Reter.Domain.UserAgg
+{
+    public class User:DomainBase<string>
+    {
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Phone { get; private set; }
+        public string UserName { get; private set; }
+        public string Email { get; private set; }
+        public int Status  { get; private set; }
+        public User(string firstName, string lastName, string phone, string userName, string email)
+        {
+            Id = Guid.NewGuid().ToString();
+            Status = Statuses.New;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            UserName = userName;
+            Email = email;
+        }
+
+        public void Active()
+        {
+            Status = Statuses.Active;
+        }
+
+        public void DeActive()
+        {
+            Status = Statuses.DeActive;
+        }
+
+        public void Ben()
+        {
+           Status = Statuses.Ben;
+        }
+    }
+}
